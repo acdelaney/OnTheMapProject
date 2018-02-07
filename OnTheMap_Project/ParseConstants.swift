cd5a39308a53428a11eea5823ce636a530cfb397
Andrew Delaney <acdelane@gmail.com>
2018-02-06 21:02:37 -0800

    Updated Constants

--- OnTheMap_Project/ParseConstants.swift
+++ OnTheMap_Project/ParseConstants.swift
@@ -41,8 +41,8 @@ struct ParseConstants {
     }
     
     struct HeaderFieldsValues {
-        static let ApplicationID = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
-        static let RESTAPIKey = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
+        static let ApplicationID = "DUMMY_VALUE"
+        static let RESTAPIKey = "DUMMY_VALUE"
         static let ContentType = "application/json"
         
     }
